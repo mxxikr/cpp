80de68b00d023e848cb3036b71212e90181fbe80
mxxikr <mxxikr@gmail.com>
2024-07-02 15:15:38 +0900

    따베시 0강 - 솔루션, 프로젝트 생성

---
+++ 홍정모CPP/MyFirstHelloWorldSolution/MyFirstHelloWorld/MyFirstHelloWorld.cpp
@@ -0,0 +1,7 @@
+#include <iostream> // 화면에 출력할 수 있는 기능을 제공하는 헤더 파일을 포함
+
+int main() // 프로그램의 시작점
+{
+	std::cout << "Hello, World!" << std::endl; // 화면에 "Hello, World!"를 출력
+	return 0; // 프로그램을 종료하고 0을 반환
+}

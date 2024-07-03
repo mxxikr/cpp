db08dffb72472465628fb2750d28651acc8c0121
mxxikr <mxxikr@gmail.com>
2024-07-03 18:19:24 +0900

    주석 보강

--- 홍정모CPP/TBCppStudy/Chapter1_6/main_1_7.cpp
+++ 홍정모CPP/TBCppStudy/Chapter1_6/main_1_7.cpp
@@ -1,23 +1,24 @@
 #include <iostream>
 
 using namespace std;
-int main() 
+
+int main()
 {
 	int x(0);
 	cout << x << " " << &x << endl; // x의 메모리 주소 값 출력
 
 	{
-		x = 1;
+		x = 1; // x 재할당
 		cout << x << " " << &x << endl; // x의 메모리 주소 값 출력
 
-		int x = 3;
-		cout << x << " " << &x << endl; // x의 메모리 주소 값 출력
+		int x = 3;  // 지역 변수
+		cout << x << " " << &x << endl; // 지역 변수 x의 메모리 주소 값 출력
 	}
 	cout << x << " " << &x << endl; // x의 메모리 주소 값 출력
 
 	{
-		int x = 2;
-		cout << x << " " << &x << endl; // x의 메모리 주소 값 출력
+		int x = 2; // 지역 변수
+		cout << x << " " << &x << endl; // 지역 변수 x의 메모리 주소 값 출력
 	}
 
 	return 0;

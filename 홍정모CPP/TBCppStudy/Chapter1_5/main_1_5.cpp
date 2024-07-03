671a986e1ea5d0c7a91a109c251f2e1616034649
mxxikr <mxxikr@gmail.com>
2024-07-03 16:58:09 +0900

    따베시 1.5강 - 함수

---
+++ 홍정모CPP/TBCppStudy/Chapter1_5/main_1_5.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+void print()
+{
+	cout << "Hello" << endl;
+}
+
+int addTwoNumbers(int num_a, int num_b)
+{
+	int sum = num_a + num_b;
+
+	return sum;
+}
+
+int multiplyTwoNumbers(int num_a, int num_b)
+{
+	int multiply = num_a * num_b;
+
+	return multiply;
+}
+
+void printHelloWorld() { // return할 값이 없을 경우 void 사용
+	cout << "Hello World!" << endl;
+	print();
+	return;
+}
+
+int main_1()
+{
+	cout << multiplyTwoNumbers(1, 2) << endl; // argrument(인자) 입력
+	cout << multiplyTwoNumbers(3, 4) << endl;
+	cout << multiplyTwoNumbers(8, 13) << endl;
+	printHelloWorld();
+
+	return 0;
+}
---
+++ 홍정모CPP/TBCppStudy/Chapter1_5/main_1_5_ex.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+int calculator(int x, int y, std::string z) {
+	int result = 0;
+	if (z == "+") {
+		result = x + y;
+	}
+	else if (z == "-") {
+		result = x - y;
+	}
+	else if (z == "*") {
+		result = x * y;
+	}
+	else if (z == "/") {
+		result = x / y;
+	}
+	else if (z == "%") {
+		result = x % y;
+	}
+	else {
+		std::cout << "Invalid value" << std::endl;
+		return 0;
+	}
+
+	std::cout << x << " " << z << " " << y << " = " << result << std::endl;
+}
+
+int main() {
+	calculator(1, 2, "+");
+	calculator(3, 4, "-");
+	calculator(8, 13, "*");
+	calculator(10, 2, "/");	
+	calculator(10, 2, "%");	
+}
